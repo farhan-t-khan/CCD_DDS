e572a425e04839beb99b8e456ed70a791e3fb17c
Farhan Khan <Farhan@Bascomturner.com>
2024-03-12 09:41:39 -0400

    Add BindingProxy and Inverse visibility parameter for Measured Concentration and Status

--- CCD_DDS/BooleanToVisibilityConverter.cs
+++ CCD_DDS/BooleanToVisibilityConverter.cs
@@ -5,26 +5,49 @@ using System.Windows.Data;
 using System.Windows.Media;
 namespace CCD_DDS
 {
+    /*    public class BooleanToVisibilityConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is bool boolValue)
+                {
+                    return boolValue ? Visibility.Visible : Visibility.Collapsed;
+                }
+
+                return Visibility.Collapsed; // Default to collapsed if value is not a bool
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is Visibility visibility)
+                {
+                    return visibility == Visibility.Visible;
+                }
+
+                return false; // Default to false if value is not a Visibility
+            }
+        }*/
     public class BooleanToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool boolValue)
             {
-                return boolValue ? Visibility.Visible : Visibility.Collapsed;
+                if (parameter != null && parameter.ToString() == "Inverse")
+                {
+                    return boolValue ? Visibility.Collapsed : Visibility.Visible;
+                }
+                else
+                {
+                    return boolValue ? Visibility.Visible : Visibility.Collapsed;
+                }
             }
-
-            return Visibility.Collapsed; // Default to collapsed if value is not a bool
+            return DependencyProperty.UnsetValue;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is Visibility visibility)
-            {
-                return visibility == Visibility.Visible;
-            }
-
-            return false; // Default to false if value is not a Visibility
+            throw new NotImplementedException();
         }
     }
 }
